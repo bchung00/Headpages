df9891ad70dab9e2ae8bc53ecadcbe2f70b13de2
Adle <adlekerhuechiam@gmail.com>
2018-11-12 17:34:35 -0800

    Populated group_contains_post table

--- database_population.sql
+++ database_population.sql
@@ -165,6 +165,32 @@ INSERT INTO groups (GID, Name, Descriptor, Type, FounderID, Founder)
             'Donkey');
 
 
+     INSERT INTO groups_contains_posts(GID, PostID)
+        VALUE
+            (1, 11),
+            (2, 12),
+            (3, 13),
+            (4, 14),
+            (5, 15),
+            (6, 16),
+            (7, 17),
+            (8, 18),
+            (9, 19),
+            (10, 20),
+            (1, 21),
+            (2, 22),
+            (3, 23),
+            (4, 24),
+            (5, 25),
+            (6, 26),
+            (7, 27),
+            (8, 28),
+            (9, 29),
+            (10, 30);
+
+
+
+
 
 
 
