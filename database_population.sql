3104c6e300aafe801ea1905936548ed16858b491
carrie <16302010062@fudan.edu.cn>
2018-11-13 22:11:00 -0800

    Fixed all the bugs in the population statements.

--- database_population.sql
+++ database_population.sql
@@ -91,7 +91,7 @@ INSERT INTO follow (UID, CID, FollowDate)
 
 -- Part B: Tables: groups, groups_contains_posts, groups_contains_user, likes, posts, users_comment_posts
 -- TODO
-INSERT INTO groups (GID, Name, Descriptor, Type, FounderID, Founder)
+INSERT INTO groups (GID, Name, Description, Type, FounderID, Founder)
     VALUES
      (1,
         'Vancouver Sneakhead Group',
@@ -165,178 +165,152 @@ INSERT INTO groups (GID, Name, Descriptor, Type, FounderID, Founder)
             'Donkey');
 
 
-     INSERT INTO groups_contains_posts(GID, PostID)
-        VALUE
-            (1, 11),
-            (2, 12),
-            (3, 13),
-            (4, 14),
-            (5, 15),
-            (6, 16),
-            (7, 17),
-            (8, 18),
-            (9, 19),
-            (10, 20),
-            (1, 21),
-            (2, 22),
-            (3, 23),
-            (4, 24),
-            (5, 25),
-            (6, 26),
-            (7, 27),
-            (8, 28),
-            (9, 29),
-            (10, 30);
 
-     INSERT INTO groups_contains_user(UID, GID)
-        VALUE
-            (1, 1),
-            (2, 2),
-            (3, 3),
-            (4, 4),
-            (5, 5),
-            (6, 6),
-            (7, 7),
-            (8, 8),
-            (9, 9),
-            (10, 10),
-            (1, 2),
-            (2, 3),
-            (3, 4),
-            (4, 5),
-            (5, 6),
-            (6, 7),
-            (7, 8),
-            (8, 9),
-            (9, 10),
-            (10, 1);
+INSERT INTO post(PostID, UID, Location, Time, Date, Photo_File, Text)
+    VALUES
+        (1, 1, 'Vancouver, BC, Canada', '16:10:20', '2018-01-01', '/photos/post1/jpeg', 'First photo ever!' ),
+        (2, 2, 'Yellow Knife, BC, Canada', '21:11:30', '2018-01-02', '/photos/post2/jpeg', 'Saw the northen lights for the first time'),
+        (3, 3, 'Toronto, ON, Canada', '13:11:11', '2018-01-03', NULL, 'Excited to see the blue jays game tonight'),
+        (4, 4, 'Vancouver, BC, Canada', '00:01:50', '2018-01-04', NULL, 'Why am i still awake at this time?'),
+        (5, 5, 'Cambridge, ON, Canada','08:12:17', '2018-01-05', '/photos/post5/jpeg', 'Great view'),
+        (6, 6, 'Montreal, QB, Canada', '09:15:40', '2018-01-06', NULL, 'Anyone can help me fix my bike?'),
+        (7, 7, 'Queens, ON, Canada', '17:12:30', '2018-01-07', '/photos/post7/jpeg', 'I regreted that cup of coffee earlier'),
+        (8, 8, 'Hamilton, ON, Canada', '22:12:21', '2018-01-08', NULL, 'I LOVE MY LIFE RN'),
+        (9, 9, 'Victoria, BC, Cadana', '01:01:01', '2018-01-09', '/photos/post9/jpeg', 'I LOVE MY DOG'),
+        (10, 10, 'Ottawa, ON, Canada','03:12:42', '2018-01-10', NULL, 'GOD BLESS CANADA'),
 
-     INSERT INTO likes(UID, PostID)
-        VALUE
-            (1, 1),
-            (1, 3),
-            (1, 12),
+        (11, 1, 'Calgary, AB, Canada', '05:12:10', '2018-02-02', NULL, 'Beautiful day in Calgary'),
+        (12, 2, 'St.John, NB, Canada', '12:12:12', '2018-11-11', NULL, 'Bless the weather in New Brunswick'),
+        (13, 3, 'Saskatoon, SK, Canada', '17:17:28', '2018-03-03', '/phots/post12/jpeg', 'Beautiful Saskatoon'),
+        (14, 4, 'Edmonton, AB, Canada', '19:39:59', '2018-04-04', NULL, 'Oops did I missed April Fools Day?'),
+        (15, 5, 'Winnepeg, MN, Canada', '12:42:12', '2018-05-05', '/photos/post15/jpeg', 'I love fall weather!'),
+        (16, 6, 'Kelowna, BC, Canada', '22:42:12', '2018-06-06', NULL, 'I love being in wine city of canada' ),
+        (17, 7, 'Guelph, ON, Canada','09:12:32', '2018-07-07', NULL, 'Summer in Guelph is the best'),
+        (18, 8, 'Regina, SK, Canada', '08:15:40', '2018-08-08', NULL, 'CANT WAIT FOR THE NEW SEMESTER!!'),
+        (19, 9, 'London, ON, Canada','12:12:32', '2018-09-09', '/photo/post19/jpeg', 'Cant believe summer is almost over'),
+        (20, 10, 'Halifax, NS, Canada', '23:23:12', '2018-10-10', '/photo/post20/jpeg', 'Winter Ball Charity Fundraising'),
 
-            (2, 2),
-            (2, 4),
-            (2, 13),
+        (21, 1, 'Vancouver, BC, Canada', '13:31:33', '2018-01-20',NULL, 'Welcome to Vancouver Sneakerhead Group'),
+        (22, 2, 'Prince Edward Island, NS, Canada','12:12:12','2018-02-20','/photo/post22/jpeg','Today at Headpages HQ'),
+        (23, 3, 'Toronton, ON, Canada', '14:43:21','2018-03-20', NULL, 'Which anime are you guys most looked forward for this summer?'),
+        (24, 4, 'Ottawa, ON, Canada','12:43:12','2018-04-20','/photo/post24/jpeg','Our very first jam night this friday, JOIN US!'),
+        (25, 5, 'Edmonton, AB, Canada', '12:54:12','2018-05-20', NULL, 'Looking for a gym buddy at 24HR fitness downtown Edmonton. hmu'),
+        (26, 6, 'St.Johns, NB, Canada', '15:32:12','2018-06-20', '/photo/post26/jpeg', 'Come out this friday to learn about bitcoin!'),
+        (27, 7, 'Calgary, AB, Canada', '23:12:12', '2018-07-20', NULL, 'Anyone suggestions on how to get started with trading forex?'),
+        (28, 8, 'Toronto, ON, Canada', '12:42:12', '2018-08-20', '/photo/post28/jpeg', 'Had a great pre-game signing and interacting with fans'),
+        (29, 9, 'Vancouver, BC, Canada', '17:32:12', '2018-09-20', NULL, 'Coming to Vancouver this friday, any suggestions where to get good asian food?'),
+        (30, 10, 'Vancouve, BC, Canada', '14:13:23', '2018-10-20', '/photo/post30/jpeg','Thank you for the volunteers and everyone that came to help out with the fundraising today');
 
-            (3, 3),
-            (3, 5),
-            (3, 14),
+INSERT INTO group_contains_post(GID, PostID)
+    VALUES
+        (1, 11),
+        (2, 12),
+        (3, 13),
+        (4, 14),
+        (5, 15),
+        (6, 16),
+        (7, 17),
+        (8, 18),
+        (9, 19),
+        (10, 20),
+        (1, 21),
+        (2, 22),
+        (3, 23),
+        (4, 24),
+        (5, 25),
+        (6, 26),
+        (7, 27),
+        (8, 28),
+        (9, 29),
+        (10, 30);
 
-            (4, 4),
-            (4, 6),
-            (4, 15),
+INSERT INTO group_contains_user(UID, GID)
+    VALUES
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+        (9, 9),
+        (10, 10),
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 5),
+        (5, 6),
+        (6, 7),
+        (7, 8),
+        (8, 9),
+        (9, 10),
+        (10, 1);
 
-            (5, 5),
-            (5, 7),
-            (5, 16),
+INSERT INTO likes(UID, PostID)
+    VALUES
+        (1, 1),
+        (1, 3),
+        (1, 12),
 
-            (6, 6),
-            (6, 8),
-            (6, 17),
+        (2, 2),
+        (2, 4),
+        (2, 13),
 
-            (7, 7),
-            (7, 9),
-            (7, 18),
+        (3, 3),
+        (3, 5),
+        (3, 14),
 
-            (8, 8),
-            (8, 1),
-            (8, 19),
+        (4, 4),
+        (4, 6),
+        (4, 15),
 
-            (9, 9),
-            (9, 2),
-            (9, 20),
+        (5, 5),
+        (5, 7),
+        (5, 16),
 
-            (10, 10),
-            (10, 3),
-            (10, 11);
+        (6, 6),
+        (6, 8),
+        (6, 17),
 
-    INSERT INTO post(PostID, UID, Location, Time, Date, Photo_File, Text)
-        VALUE
-            (1, 1, 'Vancouver, BC, Canada', '16:10:20', '2018-01-01', '/photos/post1/jpeg', 'First photo ever!' ),
-            (2, 2, 'Yellow Knife, BC, Canada', '21:11:30', '2018-01-02', '/photos/post2/jpeg', 'Saw the northen lights for the first time'),
-            (3, 3, 'Toronto, ON, Canada', '13:11:11', '2018-01-03', NULL, 'Excited to see the blue jays game tonight'),
-            (4, 4, 'Vancouver, BC, Canada', '00:01:50', '2018-01-04', NULL, 'Why am i still awake at this time?'),
-            (5, 5, 'Cambridge, ON, Canada','08:12:17', '2018-01-05', '/photos/post5/jpeg', 'Great view'),
-            (6, 6, 'Montreal, QB, Canada', '09:15:40', '2018-01-06', NULL, 'Anyone can help me fix my bike?'),
-            (7, 7, 'Queens, ON, Canada', '17:12:30', '2018-01-07', '/photos/post7/jpeg', NULL, 'I regreted that cup of coffee earlier'),
-            (8, 8, 'Hamilton, ON, Canada', '22:12:21', '2018-01-08', NULL, 'I LOVE MY LIFE RN'),
-            (9, 9, 'Victoria, BC, Cadana', '01:01:01', '2018-01-09', '/photos/post9/jpeg', 'I LOVE MY DOG'),
-            (10, 10, 'Ottawa, ON, Canada','03:12:42', '2018-01-10', NULL, 'GOD BLESS CANADA'),
+        (7, 7),
+        (7, 9),
+        (7, 18),
 
-            (11, 1, 'Calgary, AB, Canada', '05:12:10', '2018-02-02', NULL, 'Beautiful day in Calgary'),
-            (12, 2, 'St.John, NB, Canada', '12:12:12', '2018-11-11', NULL, 'Bless the weather in New Brunswick'),
-            (13, 3, 'Saskatoon, SK, Canada', '17:17:28', '2018-03-03', '/phots/post12/jpeg', 'Beautiful Saskatoon'),
-            (14, 4, 'Edmonton, AB, Canada', '19:39:59', '2018-04-04', NULL, 'Oops did I missed April Fools Day?'),
-            (15, 5, 'Winnepeg, MN, Canada', '12:42:12', '2018-05-05', '/photos/post15/jpeg', 'I love fall weather!'),
-            (16, 6, 'Kelowna, BC, Canada', '22:42:12', '2018-06-06', NULL, 'I love being in wine city of canada' ),
-            (17, 7, 'Guelph, ON, Canada','09:12:32', '2018-07-07', NULL, 'Summer in Guelph is the best'),
-            (18, 8, 'Regina, SK, Canada', '08:15:40', '2018-08-08', NULL, 'CANT WAIT FOR THE NEW SEMESTER!!'),
-            (19, 9, 'London, ON, Canada','12:12:32', '2018-09-09', '/photo/post19/jpeg', 'Cant believe summer is almost over'),
-            (20, 10, 'Halifax, NS, Canada', '23:23:12', '2018-10-10', '/photo/post20/jpeg', 'Winter Ball Charity Fundraising'),
+        (8, 8),
+        (8, 1),
+        (8, 19),
 
-            (21, 1, 'Vancouver, BC, Canada', '13:31:33', '2018-01-20',NULL, 'Welcome to Vancouver Sneakerhead Group'),
-            (22, 2, 'Prince Edward Island, NS, Canada','12:12:12','2018-02-20','/photo/post22/jpeg','Today at Headpages HQ'),
-            (23, 3, 'Toronton, ON, Canada', '14:43:21','2018-03-20', NULL, 'Which anime are you guys most looked forward for this summer?'),
-            (24, 4, 'Ottawa, ON, Canada','12:43:12','2018-04-20','/photo/post24/jpeg','Our very first jam night this friday, JOIN US!'),
-            (25, 5, 'Edmonton, AB, Canada', '12:54:12','2018-05-20', NULL, 'Looking for a gym buddy at 24HR fitness downtown Edmonton. hmu'),
-            (26, 6, 'St.Johns, NB, Canada', '15:32:12','2018-06-20', '/photo/post26/jpeg', 'Come out this friday to learn about bitcoin!'),
-            (27, 7, 'Calgary, AB, Canada', '23:12:12', '2018-07-20', NULL, 'Anyone suggestions on how to get started with trading forex?'),
-            (28, 8, 'Toronto, ON, Canada', '12:42:12', '2018-08-20', '/photo/post28/jpeg', 'Had a great pre-game signing and interacting with fans'),
-            (29, 9, 'Vancouver, BC, Canada', '17:32:12', '2018-09-20', NULL, 'Coming to Vancouver this friday, any suggestions where to get good asian food?'),
-            (30, 10, 'Vancouve, BC, Canada', '14:13:23', '2018-10-20', '/photo/post30/jpeg','Thank you for the volunteers and everyone that came to help out with the fundraising today');
+        (9, 9),
+        (9, 2),
+        (9, 20),
 
-        INSERT INTO user_comments_posts(CommentID, UID, PostID, Text)
-            VALUE
-                (1, 1, 2, 'Awesome'),
-                (2, 2, 3, 'Me too! See you there!'),
-                (3, 3, 4, 'Go to sleep bro!'),
-                (4, 4, 5, 'Nice shot!'),
-                (5, 5, 6, 'There''s a bike shops near you that my friend own, let me send you the address!'),
-                (6, 6, 7, 'I know that feel :)' ),
-                (7, 7, 8, 'Proud of you buddy!');
-                (8 ,8 ,9, 'Such a cute dog you have there!'),
-                (9, 9, 10, 'Canada is the best!'),
-                (10, 10, 11, 'Such a beautiful view in your backyard!'),
-                (11, 11, 12, 'Keep warm buddy!'),
-                (12, 12, 13, 'Amazing!'),
-                (13, 13, 14, 'Haha that was 3 days ago girl!'),
-                (14, 14, 15, 'My favourite season too!'),
-                (15, 15, 16, 'Kelowna is the best!'),
-                (16, 16, 17, 'Cant''t believe it''s summer already'),
-                (17, 17, 18, 'I''m excited and anxious at the same time!'),
-                (18, 18, 19, 'Time to whip out the skis and snowboards!'),
-                (19, 19, 20, 'Thanks everyone that came out to help!');
+        (10, 10),
+        (10, 3),
+        (10, 11);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO user_comments_posts(CommentID, UID, PostID, Text)
+    VALUES
+        (1, 1, 2, 'Awesome'),
+        (2, 2, 3, 'Me too! See you there!'),
+        (3, 3, 4, 'Go to sleep bro!'),
+        (4, 4, 5, 'Nice shot!'),
+        (5, 5, 6, 'There''s a bike shops near you that my friend own, let me send you the address!'),
+        (6, 6, 7, 'I know that feel :)' ),
+        (7, 7, 8, 'Proud of you buddy!'),
+        (8 ,8 ,9, 'Such a cute dog you have there!'),
+        (9, 9, 10, 'Canada is the best!'),
+        (10, 10, 11, 'Such a beautiful view in your backyard!'),
+        (11, 11, 12, 'Keep warm buddy!'),
+        (12, 12, 13, 'Amazing!'),
+        (13, 13, 14, 'Haha that was 3 days ago girl!'),
+        (14, 14, 15, 'My favourite season too!'),
+        (15, 15, 16, 'Kelowna is the best!'),
+        (16, 16, 17, 'Cant''t believe it''s summer already'),
+        (17, 17, 18, 'I''m excited and anxious at the same time!'),
+        (18, 18, 19, 'Time to whip out the skis and snowboards!'),
+        (19, 19, 20, 'Thanks everyone that came out to help!');
+     
 -- Part C: Tables: games, play, events, creates_events, admins, admins_monitors_content, rsvp
 
 INSERT INTO game (Name, Type, Description)
@@ -372,11 +346,11 @@ INSERT INTO creates_event (UID, Name, Time, Date, Location, Description)
     (8, 'Science of Cocktails 2019', '20:00:00', '2019-2-7', 'Science World', 'At Science of Cocktails, the city’s most talented bartenders and chefs will showcase the chemistry, biology and physics behind preparing modern cocktails and cuisine.'),
     (8, 'Whistler Opening Weekend Trip', '15:00:00', '2018-11-23', 'Whistler Blackcomb', 'Another year, another ski season at Whistler Blackcomb! Join us for opening weekend and shred some early season pow.'),
     (20, 'SpaceX At UBC', '09:00:00', '2018-11-24', 'UBC', 'Come and know about the latest technology and news about SpaceX!'),
-    (2, 'UBC Local Hack Day 2018', '09:00:00', '2018-12-1', 'Hugh Dempster Pavilion', 'Join us for UBC\'s third iteration of Local Hack Day!'),
+    (2, 'UBC Local Hack Day 2018', '09:00:00', '2018-12-1', 'Hugh Dempster Pavilion', "Join us for UBC's third iteration of Local Hack Day!"),
     (7, 'Free iOS Essentials Workshop', '18:00:00', '2018-11-23', 'Devhub', 'Build an iOS app in 2 hours!'),
-    (4, 'Live SHow', '19:30:00', '2018-11-22', 'Koerner\'s Pub on UBC Campus', 'Coming at you from Koerner’s Pub, we’ll be blowing the roof off with a night of live music, and cheap booze.'),
-    (9, 'Annual Customer Appreciation Event', '12:00:00', '2018-11-30', 'Molly\'s Seaside Market', ' Let us show our appreciation with spectacular sales, drinks, appies, gift bags, draws and as always...GOOD VIBES. Hope to see you there!'),
-    (6, 'BCom Pancake Breakfast', '09:30:00', '2018-12-13', 'Stop by CPA Hall to grab a delicious, free pancake breakfast and learn about the programming and services available at the BCC, CLC and UGO.');
+    (4, 'Live SHow', '19:30:00', '2018-11-22', "Koerner's Pub on UBC Campus", 'Coming at you from Koerner’s Pub, we’ll be blowing the roof off with a night of live music, and cheap booze.'),
+    (9, 'Annual Customer Appreciation Event', '12:00:00', '2018-11-30', "Molly's Seaside Market", ' Let us show our appreciation with spectacular sales, drinks, appies, gift bags, draws and as always...GOOD VIBES. Hope to see you there!'),
+    (6, 'BCom Pancake Breakfast', '09:30:00', '2018-12-13','CPA Hall', 'Stop by CPA Hall to grab a delicious, free pancake breakfast and learn about the programming and services available at the BCC, CLC and UGO.');
 
 INSERT INTO admins (Name, Title, Password, Username)
     VALUES
