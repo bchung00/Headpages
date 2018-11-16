9cede97226cb761222743f96e25b8df10659879f
Brandon Chung <b.chung8612@gmail.com>
2018-11-15 17:47:48 -0800

    changed connect.php

--- PJ2/connect.php
+++ PJ2/connect.php
@@ -5,7 +5,7 @@
  * Date: 2017/6/25
  * Time: 上午10:13
  */
-$db = mysqli_connect("localhost", "carrie", "carrie", "Headpages");
+$db = mysqli_connect("localhost", "root", "", "Headpages");
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit(500);
